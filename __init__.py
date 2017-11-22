92ad174a09a1485a6ab5b082b84e4fc75a1b240c
Adam Romano <manoadamro.imp@gmail.com>
2017-11-22 16:14:11 +0000

    added some methods to generator

--- __init__.py
+++ __init__.py
@@ -1,13 +1,61 @@
-from datafaker import Faker
+from generator import Generator
+from pprint import pprint
 
-FAKER = Faker()
+GEN = Generator()
 
-def identity():
-    return FAKER.dict(
+def identifier():
+    return GEN.dict(
         uuid='uuid4',
         uri='uri',
         created='date_time_ad',
         modified='date_time_ad'
     )
 
-pprint(identity())
+def address():
+    return GEN.dict(
+        address_line_1='building_number',
+        address_line_2='street_name',
+        address_line_3=None,
+        address_line_4=None,
+        locality='city',
+        region=None,
+        postcode='postcode',
+        country='override_country'
+    )
+
+def personal_address():
+    return GEN.dict(
+        address=address(),
+        lived_from='date',
+        lived_until='date',
+        status='boolean'
+    )
+
+def user():
+    return GEN.dict(
+        identifier=identifier(),
+        first_name='first_name_female',
+        last_name='last_name_female',
+        dob='date',
+        phone_number='phone_number',
+        address=personal_address(),
+        nhs_number='random_number',
+        email_address='email',
+        ethnicity='ethnicity',
+        sex='sex',
+        gender='gender'
+    )
+
+def note():
+    return GEN.dict(
+        identifier=identifier(),
+        creator=identifier(),
+        content='text'
+    )
+
+def array_of(method, count=5):
+    array = [method() for i in range(0, count)]
+    return array
+
+# pprint(identifier())
+pprint(array_of(user))
---
+++ generator.py
@@ -0,0 +1,85 @@
+from faker import Faker as FakerBase
+from random import choice
+from json import dumps
+
+
+class Types(object):
+
+        ethnicity = [
+            'white_british',
+            'black_british',
+        ]
+
+        sex = [
+            'male',
+            'female'
+        ]
+
+        gender = [
+            'male',
+            'female',
+            'Asexual',
+            'Female to male trans man',
+            'gender neutral',
+            'hermaphrodite',
+            'intersex person',
+            'male to female trans woman',
+            'polygender',
+            'other'
+        ]
+
+        override_country = [
+            'United Kingdom'
+        ]
+
+class Generator(Types):
+
+    base = FakerBase('en_GB')
+
+    def _resolve(self, name):
+        if hasattr(self.base, name):
+            return getattr(self.base, name)()
+        else:
+            method = getattr(self, name)
+            if callable(method):
+                return method()
+            else:
+                return choice(method)
+
+    def __call__(self, name):
+        if name is None or isinstance(name, dict): return name
+        item = self._resolve(name)
+        if not isinstance(item, (int, float, bool)) and item is not None:
+            return str(item)
+        else:
+            return item
+
+    def list(self, *attrs):
+        '''
+        f.list('name', 'address', 'medicine', 'units')
+        # Rachel Jones
+        # Unit 4389 Box 5768
+        # DPO AP 92072-9929
+        # codeine
+        # mmol/l
+        '''
+        return [self(i) for i in attrs]
+
+    def dict(self, **attrs):
+        '''
+        f.dict(some_name='name', some_address='address')
+        # {'some_address': '337 Kimberly Drives Suite 567\n'
+        #                  'South Stephanie, SC 30383-6482',
+        #  'some_name': 'Isaiah White'}
+        '''
+        d = {}
+        for attr in attrs: d[attr] = self(attrs[attr])
+        return d
+    #
+    # def json(self, **attrs):
+    #     '''
+    #     f.json(some_name='name', some_address='address')
+    #     # ('{"some_name": "William House", "some_address": "862 Jose '
+    #     #  'Mills\\nJohnnymouth, AK 08515-2330"}')
+    #     '''
+    #     return dumps(self.dict(**attrs))
