f636e5ee514712e2e8ec3e316a20aa42005ae5f8
Adam Romano <manoadamro.imp@gmail.com>
2017-11-22 16:16:39 +0000

    moved types to its own file

--- generator.py
+++ generator.py
@@ -1,36 +1,8 @@
 from faker import Faker as FakerBase
+from types import Types
+
 from random import choice
 from json import dumps
-
-
-class Types(object):
-
-        ethnicity = [
-            'white_british',
-            'black_british',
-        ]
-
-        sex = [
-            'male',
-            'female'
-        ]
-
-        gender = [
-            'male',
-            'female',
-            'Asexual',
-            'Female to male trans man',
-            'gender neutral',
-            'hermaphrodite',
-            'intersex person',
-            'male to female trans woman',
-            'polygender',
-            'other'
-        ]
-
-        override_country = [
-            'United Kingdom'
-        ]
 
 class Generator(Types):
 
---
+++ types.py
@@ -0,0 +1,28 @@
+class Types(object):
+
+        ethnicity = [
+            'white_british',
+            'black_british',
+        ]
+
+        sex = [
+            'male',
+            'female'
+        ]
+
+        gender = [
+            'male',
+            'female',
+            'Asexual',
+            'Female to male trans man',
+            'gender neutral',
+            'hermaphrodite',
+            'intersex person',
+            'male to female trans woman',
+            'polygender',
+            'other'
+        ]
+
+        override_country = [
+            'United Kingdom'
+        ]
